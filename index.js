a2ac139f4769a057e2dfe6950aeae21112f26a66
KimJeongHyun <ptr1129@hanmail.net>
2021-06-17 19:56:02 +0900

    separate router, create login frame

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const userRouter = require('./router/user.js');
 const port = 5000;
 
-app.get('/',(req,res)=>res.send('Server Start'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended : true})) 
+app.use(userRouter);
+
+app.get('/',(req,res)=>res.sendFile(path.resolve("infoHTML/info.html")));
 
 app.listen(port, ()=>console.log(`Server Start. Port : ${port}`))
 
 const mysql = require('./database')();
-
 const connection = mysql.init();
-
 mysql.db_open(connection);
 
-connection.query('SELECT * FROM  USERS', function (error, results, fields){
-    if (error){
-        console.log(error);
-    }
-    console.log(results);
-})
