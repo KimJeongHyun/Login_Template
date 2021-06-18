bdb54ba5273f447544019fbd47e8e3ce2f12c778
KimJeongHyun <ptr1129@hanmail.net>
2021-06-18 21:51:13 +0900

    Basic frame preparation

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const userRouter = require('./router/user.js');
+const userLogin = require('./controller/userLogin.js');
+const userRegister = require('./controller/userRegister.js');
 const port = 5000;
 
 app.use(express.json()); 
-app.use(express.urlencoded({ extended : true})) 
+app.use(express.urlencoded({extended : true})) 
 app.use(userRouter);
+app.use(userLogin);
+app.use(userRegister);
 
 app.get('/',(req,res)=>res.sendFile(path.resolve("infoHTML/info.html")));
 
 app.listen(port, ()=>console.log(`Server Start. Port : ${port}`))
 
-const mysql = require('./database')();
-const connection = mysql.init();
-mysql.db_open(connection);
 
