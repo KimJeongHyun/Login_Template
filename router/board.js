dd62c9ec73a20815d6abfddeb1bbb40e0c2f9639
KimJeongHyun <ptr1129@hanmail.net>
2021-07-13 20:08:41 +0900

    Update function ok. Need to secure post password. Ready to delete post function

--- router/board.js
+++ router/board.js
@@ -4,28 +4,29 @@ const mysql = require('../database')();
 const connection = mysql.init();
 mysql.db_open(connection);
 
-router.get('/list',function (req,res,next) {
-  res.redirect('/list/1')// /board로 접속요청이 들어왔을 때 1페이지로 자동으로 이동하도록 리다이렉트 해줍니다.
+router.get('/board/list',function (req,res,next) {
+  res.redirect('/board/list/1')// /board로 접속요청이 들어왔을 때 1페이지로 자동으로 이동하도록 리다이렉트 해줍니다.
 })
 
-router.get('/list/:page', function(req, res, next) {
-  var query = connection.query('select idx,name,title,content,regdate,modidate,hit from board',function(err,rows){
+router.get('/board/list/:page', function(req, res, next) {
+  var query = connection.query('select idx,name,title,content,hit from board',function(err,rows){
     if(err) console.log(err)        // 만약 에러값이 존재한다면 로그에 표시합니다.
     res.render('boardHTML/list.html', { title:'Board List',rows: rows }); // view 디렉토리에 있는 list 파일로 이동합니다.
   });
 });
 
-router.get('/list/post/:page',function(req,res,next){
-  var query = connection.query('select title,name,content from board where idx='+req.params.page,function(err,rows){
+router.get('/board/list/post/:page',function(req,res,next){
+  const page = req.params.page;
+  var query = connection.query('select idx,title,name,content from board where idx='+page,function(err,rows){
     if (err) console.log(err)
     res.render('boardHTML/post.html',{title:rows[0].title, rows:rows});
-    var query = connection.query('UPDATE board SET hit=hit+1 WHERE idx='+req.params.page, function(err,rows){
+    var query = connection.query('UPDATE board SET hit=hit+1 WHERE idx='+page, function(err,rows){
       if (err) console.log(err);
     })
   })
 })
 
-router.get('/list/write/new',function(req,res,next){
+router.get('/board/write/',function(req,res,next){
   if (typeof req.session.displayName!=='undefined'){
     res.render('boardHTML/write.html');
   }else{
@@ -49,15 +50,47 @@ router.post('/insert',(req,res)=>{
       if (err){
         console.log(err);
       }else{
-        res.send("<script>alert('작성되었습니다.'); document.location.href='/list'</script>")
+        res.send("<script>alert('작성되었습니다.'); document.location.href='/board/list'</script>")
       }
     })
   }
 })
 
-router.get('/list/post/modify',(req,res)=>{
-  res.render('hi')
+router.get('/board/update/:idx',(req,res)=>{
+  const idx = req.params.idx;
+  const query = connection.query('select idx,title,name,content from board where idx='+idx, function(err,rows){
+    if (err) console.log(err);
+    if (rows[0].name==req.session.displayName){
+      res.render('boardHTML/postDetail.html',{rows:rows});
+    }else{
+      res.send("<script>alert('작성자가 아닙니다.'); document.location.href='/board/list'</script>")
+    }
+    
+  })
 })
+
+router.post('/board/update',(req,res)=>{
+  const title = req.body.title;
+  const content = req.body.content;
+  const pw = req.body.pw;
+  const idx = req.body.idx;
+  const authSql = 'select passwd from board where idx=?';
+  const authQuery = connection.query(authSql,[idx],function(err,rows){
+    if (err) console.log(err);
+    if (pw==rows[0].passwd){
+      const updateSql = 'update board set title=?, content=?, modidate=now() where idx=?'
+      const updateQuery = connection.query(updateSql,[title,content,idx],function(err,rows){
+        if (err) console.log(err);
+        res.send("<script>alert('작성되었습니다.'); document.location.href='/board/list'</script>")
+      })
+    }else{
+      res.send("<script>alert('비밀번호가 다릅니다.'); window.history.back()</script>")
+    }
+  })
+  
+
+})
+
 
 
 module.exports = router;
