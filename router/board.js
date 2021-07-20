37bd09000b570fa7f1de76380fcf2bc2343b33ca
KimJeongHyun <ptr1129@hanmail.net>
2021-07-20 20:04:20 +0900

    recommend post function clear. (recommend, unrecommend) Search filter clear.

--- router/board.js
+++ router/board.js
@@ -16,11 +16,11 @@ router.get('/board/list/:page', function(req, res, next) {
   conn.getConnection((err,connection)=>{
     if (err) throw err;
     const query = connection.query(sql,function(err,rows){
-      if (err) console.log(err);
+      if (err) throw err;
       if (rows.length==0){
         const idxInit = 'ALTER TABLE board AUTO_INCREMENT=1;'
         const idxInitQuery = connection.query(idxInit,function(err,rows){
-          if (err) console.log(err);
+          if (err) throw err;
         })
         res.render('boardHTML/pageEmpty.html',{title:'게시판 리스트',lastidx:1});
       }else{
@@ -36,15 +36,32 @@ router.get('/board/list/post/:page',function(req,res,next){
   conn.getConnection((err,connection)=>{
     if (err) throw err;
     const query = connection.query('SELECT idx, title, name, nick, content, hit, recommend FROM board where idx='+page,function(err,rows){
-      if (err) console.log(err)
-      if (rows[0].name==req.session.displayName){
+      if (err) throw err
+      const author = rows[0].name;
+      const preRows = rows;
+      if (author==req.session.displayName){
         res.render('boardHTML/postUser.html',{title:rows[0].title, rows:rows});
       }else{
-        res.render('boardHTML/postGuest.html',{title:rows[0].title,rows:rows});
+        const recomSelSql = 'SELECT recomPost FROM users WHERE id=?';
+        const recomSelQuery = connection.query(recomSelSql,[req.session.displayName],(err,rows)=>{
+          if (err) throw err;
+          if (rows[0].recomPost==null){
+            res.render('boardHTML/postGuest.html',{title:preRows[0].title,rows:preRows});
+          }else{
+            const recomPosts = rows[0].recomPost;
+            const recomPostsArray = recomPosts.split(';');
+            if (recomPostsArray.includes(page)){
+              res.render('boardHTML/postGuestRecommended.html',{title:preRows[0].title,rows:preRows});
+            }else{
+              res.render('boardHTML/postGuest.html',{title:preRows[0].title,rows:preRows});
+            }
+          }          
+        })
+        
       }
       if (req.session.refresh==true){
         const query = connection.query('UPDATE board SET hit=hit+1 WHERE idx='+page, function(err,rows){
-          if (err) console.log(err);
+          if (err) throw err;
         })
       }
       connection.release();
@@ -76,20 +93,20 @@ router.post('/insert',(req,res)=>{
         if (err) throw err;
         const selQuery = connection.query(selSql, function(err,rows){
           if (err){
-            console.log(err);
+            throw err;
           }else{
             const initQuery = connection.query(idxInit,[rows.length], function(err,rows){
-              if (err) console.log(err);
+              if (err) throw err;
             })
           }
         })
         const authQuery = connection.query(authSql,[author],function(err,rows){
           if (err){
-            console.log(err);
+            throw err;
           }else{
             const insertQuery=connection.query(insertSql,[title,content,author,rows[0].nick],function(err,rows){
               if (err){
-                console.log(err);
+                throw err;
               }else{
                 res.send("<script>alert('작성되었습니다.'); document.location.href='/board/list'</script>")
                 req.session.writeIdx="";
@@ -111,7 +128,7 @@ router.get('/board/update/:idx',(req,res)=>{
   conn.getConnection((err,connection)=>{
     if (err) throw err;
     const query = connection.query('SELECT idx, title, name, content FROM board WHERE idx='+idx, function(err,rows){
-      if (err) console.log(err);
+      if (err) throw err;
       if (rows[0].name==req.session.displayName){
         res.render('boardHTML/postDetail.html',{rows:rows});
       }else{
@@ -130,7 +147,7 @@ router.post('/board/update',(req,res)=>{
   conn.getConnection((err,connection)=>{
     if (err) throw err;
     const updateQuery = connection.query(updateSql,[title,content,idx],function(err,rows){
-      if (err) console.log(err);
+      if (err) throw err;
       res.send("<script>alert('작성되었습니다.'); document.location.href='/board/list'</script>")
       connection.release();
     })
@@ -143,10 +160,10 @@ router.get('/board/delete/:idx',(req,res)=>{
   const delSql = 'DELETE FROM board WHERE idx=?';
   conn.getConnection((err,connection)=>{
     if (err) throw err;
-    const query = connection.query(authSql,[idx],function(err,rows){
-      if (err) console.log(err);
+    const authQuery = connection.query(authSql,[idx],function(err,rows){
+      if (err) throw err;
       if (rows[0].name==req.session.displayName){
-        const query = connection.query(delSql,[idx],function(err,rows){
+        const delQuery = connection.query(delSql,[idx],function(err,rows){
           res.send("<script>alert('삭제되었습니다.'); document.location.href='/board/list'</script>")
         })
       }else{
@@ -157,22 +174,85 @@ router.get('/board/delete/:idx',(req,res)=>{
   })
 })
 
+router.get('/recommendDel/:idx',(req,res)=>{
+  if (typeof req.session.displayName!=='undefined'){
+    const idx = req.params.idx;
+    const selSql = 'SELECT recomPost FROM users WHERE id=?';
+    const updateSql = 'UPDATE users SET recomPost=? WHERE id=?';
+    const updateRecomSql = 'UPDATE board SET recommend = recommend-1 WHERE idx=?';
+    conn.getConnection((err,connection)=>{
+      if (err) throw err;
+      const query = connection.query(selSql,[req.session.displayName],(err,rows)=>{
+        if (err) throw err;
+        let recomList='';
+        const recomPosts = rows[0].recomPost;
+        const recomPostsArray = recomPosts.split(";");
+        if (recomPosts.length>1){
+          for (let i=0; i<recomPostsArray.length; i++){
+            if (recomPostsArray[i]!=idx && recomPostsArray[i]!=''){
+              recomList+=recomPostsArray[i]+';';
+            }
+          }
+        }
+        const updateQuery = connection.query(updateSql,[recomList,req.session.displayName],(err,rows)=>{
+          if (err) throw err;
+        })
+        const updateRecomQuery = connection.query(updateRecomSql,[idx],(err,rows)=>{
+          if (err) throw err;
+          connection.release();
+        })
+      })
+      res.send("<script>alert('추천 해제되었습니다.'); document.location.href='/board/list/post/"+idx+"'</script>")
+    })
+  }else{
+    res.send("<script>alert('비정상적인 접근입니다.'); window.history.back();</script>")
+  }
+})
+
 router.get('/recommend/:idx',(req,res)=>{
   if (typeof req.session.displayName!=='undefined'){
     const idx = req.params.idx;
-    const selSql = 'SELECT name from board where idx=?';
-    const recomSql = 'UPDATE board SET recommend=recommend+1 where idx=?';
+    const selSql = 'SELECT name FROM board WHERE idx=?';
+    const recomUpdateSql = 'UPDATE board SET recommend=recommend+1 WHERE idx=?';
+    const recomSelSql = 'SELECT recomPost FROM users WHERE id=?';
+    const recomPostUpdateSql = 'UPDATE users SET recomPost = ? WHERE id=?';
     conn.getConnection((err,connection)=>{
       if (err) throw err;
       const query = connection.query(selSql,[idx],function(err,rows){
-        if (err) console.log(err);
+        if (err) throw err;
         if (rows[0].name==req.session.displayName){
           res.send("<script>alert('자기 자신의 게시글에 추천을 누를 수 없습니다.'); location.reload();</script>");
         }else{
-          const query = connection.query(recomSql,[idx],function(err,rows){
-            if (err) console.log(err);
-            res.send("<script>document.location.href='/board/list/post/"+idx+"'</script>")
-          })
+          const recomSelQuery = connection.query(recomSelSql,[req.session.displayName],(err,rows)=>{
+            if (err) throw err;
+            if (rows[0].recomPost ==null){
+              const recomPostUpdateQuery = connection.query(recomPostUpdateSql,[idx+';',req.session.displayName],(err,rows)=>{
+                if (err) throw err;
+              });
+              res.send("<script>document.location.href='/board/list/post/"+idx+"'</script>")
+            }else{
+              const recomPosts = rows[0].recomPost;
+              const recomPostsArray = recomPosts.split(';');
+              if (recomPostsArray.includes(idx)){
+                res.send("<script>alert('이미 추천을 누른 게시물입니다.'); window.history.back();</script>");
+              }else{
+                let recomPostList='';
+                for (let i=0; i<recomPostsArray.length; i++){
+                  if (recomPostsArray[i]!=''){
+                    recomPostList+=recomPostsArray[i]+';';
+                  }
+                }
+                recomPostList+=idx+';';
+                const recomPostUpdateQuery = connection.query(recomPostUpdateSql,[recomPostList,req.session.displayName],(err,rows)=>{
+                  if (err) throw err;
+                });
+                const recomUpdateQuery = connection.query(recomUpdateSql,[idx],function(err,rows){
+                  if (err) throw err;
+                  res.send("<script>document.location.href='/board/list/post/"+idx+"'</script>")
+                })
+              }
+            }
+          })          
         }
         connection.release();
       })
